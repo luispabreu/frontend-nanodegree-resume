c46158e48dd623315f2513da00d026a9313abfcf
Luis Enrique Pérez Abreu Velázquez <luis.abreu@itesm.mx>
2015-11-23 19:15:53 -0600

    Add variables to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
+$("#main").append("Luis Abreu")
+
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var name ="Luis Perez Abreu";
+ var role ="FrontPage Developer";
+
+ var awesomeThoughts = "I am luis and I am awesome";
+ console.log(awesomeThoughts);
+ 
+ var funThoughts = awesomeThoughts.replace("awesome","Fun");
+ console.log(funThoughts);
+
+ $("#main").append(funThoughts);
+
+ var formattedName = HTMLheaderName.replace("%data%",name);
+ var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
